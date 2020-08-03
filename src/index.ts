0b2536d6518f3b5a48a914d247b8fe4ef5479589
Jiawei <liujiaweidreamer@gmail.com>
2020-08-03 18:49:07 +0800

    import https module to send request

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
+import * as https from "https";
+import {IncomingMessage} from "http";
+
 const translate = (word) => {
-  console.log(word);
+  const options = {
+    hostname: 'www.zhihu.com',
+    port: 443,
+    path: '/',
+    method: 'GET'
+  };
+
+  const request = https.request(options, (response: IncomingMessage) => {
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+
+    response.on('data', (data) => {
+      process.stdout.write(data);
+    });
+  });
+
+  request.on('error', (error) => {
+    console.error(error);
+  });
+  request.end();
 };
 
 export default translate;

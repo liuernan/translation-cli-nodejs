8b42383f96eedce440f4f4695fce14409d55f2f9
Jiawei <liujiaweidreamer@gmail.com>
2020-08-03 19:06:46 +0800

    import querystring module to make query string

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
 import * as https from "https";
 import {IncomingMessage} from "http";
+import * as querystring from "querystring";
 
 const translate = (word) => {
+
+  const query: string = querystring.stringify({
+    from: "eb",
+    to: "zh",
+    src: word
+  });
+  console.log(query);
+
   const options = {
-    hostname: 'www.zhihu.com',
+    hostname: "www.zhihu.com",
     port: 443,
-    path: '/',
-    method: 'GET'
+    path: "/",
+    method: "GET"
   };
 
   const request = https.request(options, (response: IncomingMessage) => {
-    console.log('statusCode:', response.statusCode);
-    console.log('headers:', response.headers);
-
-    response.on('data', (data) => {
+    response.on("data", (data) => {
       process.stdout.write(data);
     });
   });
 
-  request.on('error', (error) => {
+  request.on("error", (error) => {
     console.error(error);
   });
+
   request.end();
 };
 
